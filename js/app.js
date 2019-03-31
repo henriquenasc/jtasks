2873de4bc15190f314ca19f4562e077fb6bf44c0
Henrique Nascimento <henriquenascandrade@gmail.com>
2019-03-31 19:20:28 -0300

    add feature remove task and checked

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
+const ul = document.querySelector("ul");
 document.querySelector("button").addEventListener("click", () => {
   const tasks = document.getElementById("tasks");
-  const ul = document.querySelector("ul");
 
   let li = document.createElement("li");
   let text = document.createTextNode(tasks.value);
-
   li.appendChild(text);
   li.classList.add("do");
-  ul.appendChild(li);
 
+  let btnRemove = document.createElement("button");
+  let textRemove = document.createTextNode("remove");
+  btnRemove.appendChild(textRemove);
+  btnRemove.classList.add("deleteTask");
+  li.appendChild(btnRemove);
+
+  let ckbox = document.createElement("input");
+  ckbox.setAttribute("type", "checkbox");
+  ckbox.classList.add("ckbox");
+  li.appendChild(ckbox);
+
+  ul.appendChild(li);
   tasks.value = "";
 
-  event();
+  btnRemove.onclick = function() {
+    ul.removeChild(li);
+  };
+
+  handlerClickState();
 });
 
-const event = () => {
+const handlerClickState = () => {
   const items = document.querySelectorAll("li");
-  items.forEach(it => {
-    it.onclick = function() {
-      it.className == "do"
-        ? it.classList.add("done")
-        : it.classList.remove("done");
+  items.forEach(li => {
+    li.onchange = () => {
+      if (li.className == "do") {
+        li.classList.add("done");
+      } else {
+        li.classList.remove("done");
+      }
     };
   });
 };
